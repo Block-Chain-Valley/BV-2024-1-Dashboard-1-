9421302d1da2e336031d528d0abbcf498554a62b
sohee <soheek@fairsquarelab.com>
2024-04-02 18:06:45 +0900

    Feat: Organ 컴포넌트 구현

--- dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
+++ dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
@@ -1,3 +1,8 @@
+import s from './index.module.scss';
+import AddressStatus from '@/components/atoms/dashboard/AddressStatus';
+import Amount from '@/components/atoms/dashboard/Amount';
+import Asset from '@/components/atoms/dashboard/Asset';
+import Date from '@/components/atoms/dashboard/Date';
 import { TokenTransferStatus } from '@/libs/types';
 
 export interface SingleTransactionInfoProps {
@@ -11,5 +16,14 @@ export interface SingleTransactionInfoProps {
 }
 
 export default function SingleTransactionInfo(props: SingleTransactionInfoProps) {
-  return <div>{/* 이곳에 코드를 작성해 주세요. */}</div>;
+  const { assetAddress, symbol, name, targetAddress, status, amount, timestamp } = props;
+
+  return (
+    <div className={s.container}>
+      <Asset address={assetAddress} symbol={symbol} name={name} />
+      <AddressStatus address={targetAddress} status={status} />
+      <Amount symbol={symbol} amount={amount} />
+      <Date timestamp={timestamp} />
+    </div>
+  );
 }
