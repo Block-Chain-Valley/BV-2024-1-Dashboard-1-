cd057fa5d85f5377422d2c227db7ed2117070e70
yumzen <yeom.ji.eun35@gmail.com>
2024-04-07 19:06:32 +0900

    Feat: SingeTransactionInfo 컴포넌트 추가 (✅)

--- dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
+++ dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
@@ -1,3 +1,8 @@
+import s from './index.module.scss';
+import AddressStatus from '@/components/atoms/dashboard/AddressStatus';
+import Amount from '@/components/atoms/dashboard/Amount';
+import Asset from '@/components/atoms/dashboard/Asset';
+import DateDiff from '@/components/atoms/dashboard/Date';
 import { TokenTransferStatus } from '@/libs/types';
 
 export interface SingleTransactionInfoProps {
@@ -7,9 +12,18 @@ export interface SingleTransactionInfoProps {
   targetAddress: string;
   status: TokenTransferStatus;
   amount: string;
-  timestamp: number;
+  timestamp: Date;
 }
 
 export default function SingleTransactionInfo(props: SingleTransactionInfoProps) {
-  return <div>{/* 이곳에 코드를 작성해 주세요. */}</div>;
+  const { assetAddress, symbol, name, targetAddress, status, amount, timestamp } = props;
+
+  return (
+    <div className={s.container}>
+      <Asset address={assetAddress} symbol={symbol} name={name} />
+      <AddressStatus address={targetAddress} status={status} />
+      <Amount symbol={symbol} amount={amount} />
+      <DateDiff transactionTime={timestamp} />
+    </div>
+  );
 }
