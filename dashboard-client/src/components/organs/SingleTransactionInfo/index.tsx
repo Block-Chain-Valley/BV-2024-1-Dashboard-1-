55d63ffb8e99c794e22161ef83c5797c18c0703d
smreosms13 <smreosms13@gamil.com>
2024-04-07 16:46:16 +0900

    Feat: Add SingleTransactionInfo(organ) component

--- dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
+++ dashboard-client/src/components/organs/SingleTransactionInfo/index.tsx
@@ -1,3 +1,8 @@
+import Address from '@/components/atoms/dashboard/Address';
+import Amount from '@/components/atoms/dashboard/Amount';
+import Asset from '@/components/atoms/dashboard/Asset';
+import Date from '@/components/atoms/dashboard/Date';
+import s from '@/components/organs/SingleTransactionInfo/index.module.scss';
 import { TokenTransferStatus } from '@/libs/types';
 
 export interface SingleTransactionInfoProps {
@@ -11,5 +16,14 @@ export interface SingleTransactionInfoProps {
 }
 
 export default function SingleTransactionInfo(props: SingleTransactionInfoProps) {
-  return <div>{/* 이곳에 코드를 작성해 주세요. */}</div>;
+  const { assetAddress, symbol, name, targetAddress, status, amount, timestamp } = props;
+
+  return (
+    <div className={s.container}>
+      <Asset address={assetAddress} symbol={symbol} name={name} />
+      <Address address={targetAddress} status={status} />
+      <Amount symbol={symbol} amount={amount} />
+      <Date timestamp={timestamp} />
+    </div>
+  );
 }
