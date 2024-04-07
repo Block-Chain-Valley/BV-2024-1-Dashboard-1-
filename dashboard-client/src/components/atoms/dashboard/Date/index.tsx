a11abc1a0404d7e25df98fad4ea401e07bec276d
yumzen <yeom.ji.eun35@gmail.com>
2024-04-07 18:51:02 +0900

    Feat: Date 컴포넌트 추가 (✅)

---
+++ dashboard-client/src/components/atoms/dashboard/Date/index.tsx
@@ -0,0 +1,30 @@
+import s from './index.module.scss';
+
+export interface DateProps {
+  transactionTime: Date;
+}
+
+export default function DateDiff({ transactionTime }: DateProps) {
+  const currentTime = new Date();
+  const timeDiff = Math.floor((currentTime.getTime() - transactionTime.getTime()) / 1000);
+
+  let dateDiff: string;
+  if (timeDiff < 60) {
+    dateDiff = `${timeDiff}초 전`;
+  } else if (timeDiff < 3600) {
+    const minutes = Math.floor(timeDiff / 60);
+    dateDiff = `${minutes}분 전`;
+  } else if (timeDiff < 86400) {
+    const hours = Math.floor(timeDiff / 3600);
+    dateDiff = `${hours}시간 전`;
+  } else {
+    const days = Math.floor(timeDiff / 86400);
+    dateDiff = `${days}일 전`;
+  }
+
+  return (
+    <div className={s.container}>
+      <p className={s.date}>{dateDiff}</p>
+    </div>
+  );
+}
