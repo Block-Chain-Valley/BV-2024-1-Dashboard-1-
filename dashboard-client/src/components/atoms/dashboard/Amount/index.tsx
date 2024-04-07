8a7d06100467cf8e13e783e0ac4ca65519f10a05
smreosms13 <smreosms13@gamil.com>
2024-04-07 15:17:19 +0900

    Feat: Add Amount(atom) component

---
+++ dashboard-client/src/components/atoms/dashboard/Amount/index.tsx
@@ -0,0 +1,32 @@
+import { UIProps } from '../../props';
+import s from './index.module.scss';
+import { formatAmount, formatSymbol, isOverAmount, isUnderAmount } from '@/libs/utils';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(s);
+
+export interface AmountProps extends UIProps.Div {
+  symbol: string;
+  amount: string;
+}
+
+export default function Amount(props: AmountProps) {
+  const { symbol, amount } = props;
+
+  let amountState = '';
+  if (isOverAmount(amount)) {
+    amountState = 'over';
+  } else if (isUnderAmount(amount)) {
+    amountState = 'under';
+  }
+
+  const formattedSymbol = formatSymbol(symbol);
+  const formattedAmout = formatAmount(amount);
+
+  return (
+    <div className={s.amount_container}>
+      <p className={cx('amount', amountState)}>{formattedAmout}</p>
+      <span className={s.amount_symbol}>{formattedSymbol}</span>
+    </div>
+  );
+}
