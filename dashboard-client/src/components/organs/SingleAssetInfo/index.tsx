5a8d8defd5012ed894aac60a5ffe312a64aa35cb
yumzen <yeom.ji.eun35@gmail.com>
2024-04-07 19:05:54 +0900

    Feat: SingeAsserInfo 컴포넌트 추가 (✅)

--- dashboard-client/src/components/organs/SingleAssetInfo/index.tsx
+++ dashboard-client/src/components/organs/SingleAssetInfo/index.tsx
@@ -1,3 +1,8 @@
+import s from './index.module.scss';
+import BaseButton from '@/components/atoms/button/BaseButton';
+import Amount from '@/components/atoms/dashboard/Amount';
+import Asset from '@/components/atoms/dashboard/Asset';
+
 export interface SingleAssetInfoProps {
   address: string;
   symbol: string;
@@ -9,5 +14,21 @@ export interface SingleAssetInfoProps {
 }
 
 export default function SingleAssetInfo(props: SingleAssetInfoProps) {
-  return <div>{/* 이곳에 코드를 작성해 주세요. */}</div>;
+  const { address, symbol, name, balance, isEdit, onSendAsset, onRemoveAsset } = props;
+
+  return (
+    <div className={s.container}>
+      <div className={s.asset_container}>
+        <Asset address={address} symbol={symbol} name={name} />
+        <Amount symbol={symbol} amount={balance} />
+      </div>
+      <div className={s.button}>
+        {isEdit ? (
+          <BaseButton assert name="삭제" onClick={onRemoveAsset} />
+        ) : (
+          <BaseButton name="보내기" onClick={onSendAsset} />
+        )}
+      </div>
+    </div>
+  );
 }
