f6195f7108f78946602391c87a19b781bdca8ade
sohee <soheek@fairsquarelab.com>
2024-04-08 21:26:36 +0900

    Feat: 자산 추가 기능 구현

--- dashboard-client/src/components/popups/Modal/AddAssetModal/index.tsx
+++ dashboard-client/src/components/popups/Modal/AddAssetModal/index.tsx
@@ -3,12 +3,17 @@ import Modal from '../index';
 import s from './index.module.scss';
 import BaseButton from '@/components/atoms/button/BaseButton';
 import TextField from '@/components/atoms/inputs/TextField';
+import ValidationMessage from '@/components/atoms/inputs/ValidationMessage';
+import SingleAssetInfoModal, { SingleAssetInfoModalProps } from '@/components/organs/SingleAssetInfoModal';
 import useInputValidation from '@/hooks/useInputValidation';
+import { ERC20_ABI, getProvider } from '@/libs/provider';
+import { ValidateState } from '@/libs/validator';
 import ErrorIcon from '@/public/assets/Error.png';
 import SuccessIcon from '@/public/assets/Success.png';
-import { ModalContext, ToastContext } from '@/store/GlobalContext';
+import { ModalContext, ToastContext, UserAssetsContext, WalletContext } from '@/store/GlobalContext';
 import { useCreateAsset } from '@graphql/client';
-import { useContext, useEffect, useRef } from 'react';
+import { ethers } from 'ethers';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 
 /* 
   [HW 2-1] 자산 추가 기능 개발하기 
@@ -16,17 +21,36 @@ import { useContext, useEffect, useRef } from 'react';
   - 반드시 모든 기능을 이 파일에 작성하지 않아도 괜찮아요. 필요 시 새로운 컴포넌트를 생성하거나, libs 폴더에 새로운 유틸리티 함수를 추가해도 괜찮아요.
 */
 
+type Status = 'IDLE' | 'INVALID' | 'PENDING' | 'CAN_ADD' | 'ALREADY_EXIST' | 'NOT_FOUND';
+type ResultAsset = SingleAssetInfoModalProps & { decimal: number };
+
+const validation: Record<Status, { msg: string; isError: boolean }> = {
+  IDLE: { msg: '', isError: false },
+  INVALID: { msg: '', isError: true },
+  PENDING: { msg: '입력하신 자산을 네트워크에서 찾고 있어요.', isError: false },
+  CAN_ADD: { msg: '입력하신 자산이 맞나요?', isError: false },
+  ALREADY_EXIST: { msg: '이미 지갑에 추가된 자산이에요.', isError: true },
+  NOT_FOUND: { msg: '자산을 찾을 수 없어요. 입력하신 주소가 맞는지 다시 한 번 확인해 주세요.', isError: true },
+};
+
 export default function AddAssetModal() {
   const [, setModal] = useContext(ModalContext);
   const [, setToast] = useContext(ToastContext);
+  const { wallet } = useContext(WalletContext);
+  const [userAssets, setUserAssets] = useContext(UserAssetsContext);
+
+  const [status, setStatus] = useState<Status>('IDLE');
+  const [resultAsset, setResultAsset] = useState<ResultAsset | null>(null);
 
   const ref = useRef<HTMLInputElement>(null);
 
   /* 
     아래 코드는 입력값을 검증하는 로직을 포함하는 커스텀 훅이예요. 필요하다면 사용해도 좋아요. 
   */
-  const { input, isValidInput, inputChangeHandler } =
-    useInputValidation(/* 입력값 검증 함수 - () => boolean 타입이어야 해요. */);
+  const { input, isValidInput, inputChangeHandler } = useInputValidation((input: string) => {
+    const regex = /^0x([a-fA-F0-9]{40})$/;
+    return regex.test(input);
+  });
 
   /* 
     아래 코드는 추가하고자 하는 자산의 검증이 완료되었을 시, 서버로 추가하고자 하는 자산 정보를 보내는 코드예요.
@@ -43,22 +67,77 @@ export default function AddAssetModal() {
     },
   });
 
-  // 사용 예시
-  // const response = await createAsset({
-  //   variables: {
-  //     input: {
-  //       userWalletAddress: // 값 추가
-  //       address: // 값 추가
-  //       type: // = 'TOKEN' 으로 고정해서 넣어주세요.
-  //       name: // 값 추가
-  //       symbol: // 값 추가
-  //       decimal: // 값 추가
-  //       balance: // 값 추가
-  //     },
-  //   },
-  // });
-  // const createAssetInfo = response.data?.createAsset;
-  // if (!createAssetInfo) throw new Error();
+  const handleSearchAsset = useCallback(
+    async (address: string) => {
+      if (wallet) {
+        const provider = getProvider(wallet.provider);
+        const contract = new ethers.Contract(address, ERC20_ABI, provider);
+
+        try {
+          const [name, symbol, balance, decimals] = await Promise.all([
+            contract.name(),
+            contract.symbol(),
+            contract.balanceOf(wallet.accounts[0].address),
+            contract.decimals(),
+          ]);
+          const formattedBalance = ethers.utils.formatUnits(balance.toString(), decimals);
+          const formattedDecimal = decimals.toNumber();
+          setResultAsset({ address, name, symbol, balance: formattedBalance, decimal: formattedDecimal });
+
+          const isAlreadyExist = userAssets.find(
+            ({ assetInfo }) => assetInfo.address.toLowerCase() === address.toLowerCase()
+          );
+          if (isAlreadyExist) {
+            setStatus('ALREADY_EXIST');
+          } else {
+            setStatus('CAN_ADD');
+          }
+        } catch (e) {
+          setStatus('NOT_FOUND');
+        }
+      }
+    },
+    [userAssets, wallet]
+  );
+
+  const handleCreateAsset = useCallback(async () => {
+    if (wallet && resultAsset) {
+      try {
+        const response = await createAsset({
+          variables: {
+            input: {
+              ...resultAsset,
+              userWalletAddress: wallet.accounts[0].address,
+              type: 'TOKEN',
+            },
+          },
+        });
+        const createAssetInfo = response.data?.createAsset;
+        if (createAssetInfo) {
+          const { balance, ...assetInfo } = resultAsset;
+          setUserAssets([...userAssets, { assetInfo, balance }]);
+          setModal(null);
+        } else {
+          throw new Error();
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [wallet, resultAsset, createAsset, setModal, setUserAssets, userAssets]);
+
+  useEffect(() => {
+    setResultAsset(null);
+
+    if (input.length === 0) {
+      setStatus('IDLE');
+    } else if (isValidInput === ValidateState.VALIDATED) {
+      setStatus('PENDING');
+      handleSearchAsset(input);
+    } else {
+      setStatus('INVALID');
+    }
+  }, [input, isValidInput, handleSearchAsset]);
 
   /* 
     모달이 열렸을 때, Textfield로 포커스를 주는 코드예요. 
@@ -77,12 +156,15 @@ export default function AddAssetModal() {
           <TextField
             placeholder="여기에 자산 주소를 입력하세요."
             ref={ref}
-            value={''}
-            error={false}
-            onChange={() => {}}
+            value={input}
+            error={validation[status].isError}
+            onChange={inputChangeHandler}
           />
         </div>
-        <div className={s.modal_sub_info}>{/* 코드 추가 */}</div>
+        <div className={s.modal_sub_info}>
+          <ValidationMessage {...validation[status]} />
+          {resultAsset && <SingleAssetInfoModal {...resultAsset} />}
+        </div>
         <div className={s.modal_buttons}>
           <BaseButton
             assert={false}
@@ -91,7 +173,12 @@ export default function AddAssetModal() {
               setModal(null);
             }}
           ></BaseButton>
-          <BaseButton assert={true} name="추가하기" disabled={false} onClick={() => {}}></BaseButton>
+          <BaseButton
+            assert={true}
+            name="추가하기"
+            disabled={status !== 'CAN_ADD'}
+            onClick={handleCreateAsset}
+          ></BaseButton>
         </div>
       </div>
     </Modal>
