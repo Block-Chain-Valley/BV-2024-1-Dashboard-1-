b4349a6de66d9140b3804f73741e26975d4f2482
smreosms13 <smreosms13@gamil.com>
2024-04-29 01:52:23 +0900

    Fix: Add messageContent and fix props

--- dashboard-client/src/components/atoms/dashboard/Message/index.tsx
+++ dashboard-client/src/components/atoms/dashboard/Message/index.tsx
@@ -1,15 +1,24 @@
 import s from './index.module.scss';
+import { AddAssetModalStatus } from '@/libs/types';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(s);
 
-interface MessageProps {
-  content: string;
-  isError: boolean;
+export interface MessageProps {
+  status: AddAssetModalStatus;
 }
 
-export function Message(props: MessageProps) {
-  const { content, isError } = props;
+const messageContent: Record<AddAssetModalStatus, { content: string; isError: boolean }> = {
+  DEFAULT: { content: '', isError: false },
+  FETCHING: { content: '입력하신 자산을 네트워크에서 찾고 있어요.', isError: false },
+  AVAILABLE: { content: '입력하신 자산이 맞나요?', isError: false },
+  ALREADY_ADDED: { content: '이미 지갑에 추가된 자산이에요.', isError: true },
+  NOT_FOUND: { content: '자산을 찾을 수 없어요. 입력하신 주소가 맞는지 다시 한 번 확인해 주세요.', isError: true },
+};
+
+export default function Message(props: MessageProps) {
+  const { status } = props;
+  const { content, isError } = messageContent[status];
 
   return <p className={cx('message', isError ? 'error' : '')}>{content}</p>;
 }
